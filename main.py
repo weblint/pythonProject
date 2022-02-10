0ec8281dc36112c817f06ee6ae5ebe4eddfbd37b
weblint <klaus@loewenhagen.de>
2022-02-10 15:43:49 +0100

    add lotto

--- main.py
+++ main.py
@@ -7,7 +7,8 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
-def multi():
+
+def multi() :
     a = int(input("Einmal Eins:"))
     x = int(input("Wiederholungen: "))
     print("_________________________")
@@ -15,8 +16,19 @@ def multi():
         x = a * (i)
         print('   ', i, '    ', x)
 
+def lotto():
+    import random
+    print ('hier kommen die Lottozahlen')
+    lottozahlen_alle = []
+    lottozahlen_alle.extend(range(1, 50))
+    lottozahlen_gewinner = random.sample(lottozahlen_alle, 6)
+    lottozahlen_gewinner.sort()
+    print(lottozahlen_gewinner)
+    print(random.choices(lottozahlen_alle, k=6))
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    #print_hi('PyCharm')
+    print_hi('PyCharm')
     multi()
+    lotto()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
